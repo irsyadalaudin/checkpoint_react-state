f7f30a9ceaf20593b7c42c1385d20f09ad6cacf0
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-05-14 18:03:07 +0300

    adding clear interval and style

--- src/App.jsx
+++ src/App.jsx
@@ -5,55 +5,76 @@ class App extends Component {
         super();
         this.state = {
             visible: false,
-            number: 0
+            timeInterval: 0,
+            intervalId: null,
         }
         
         this.identitiy = {
             fullName: 'Irsyad Alaudin Hariono',
             profession: 'Software Engineer',
-            bio: 'abcd',
+            bio: 'Irsyad Alaudin Hariono is a talented software engineer with a passion for coding. He has extensive experience in developing software applications and solving complex problems. Irsyad is known for his attention to detail and his ability to work collaboratively with other team members. Throughout his career, Irsyad has worked on a variety of projects, ranging from mobile applications to complex software systems. He has a deep understanding of programming languages such as JavaScript, Python, and Java, and is always looking to expand his skill set. Irsyad is a dedicated and hard-working individual who is committed to producing high-quality work. He is constantly seeking new challenges and opportunities to grow as a software engineer. With his exceptional technical skills and his passion for technology, Irsyad is a valuable asset to any team.',
             img: 'https://picsum.photos/id/5/200/300',
         }
-    }
+    }    
 
     handleClick = ()=>{
-        this.setState((prev) =>{
+        if(!this.state.visible) {
+            this.setState({
+                intervalId : setInterval(() => {
+                    this.setState(prevState => ({
+                        timeInterval: prevState.timeInterval + 1
+                    }))
+                }, 1000)
+            });
+        } else {
+            clearInterval(this.state.intervalId);
+            this.setState({
+                timeInterval: 0
+            })
+            this.setState({
+                intervalId: null
+            });
+        }
+
+        this.setState((prev) => {
             return {visible: !prev.visible}
         })
     }
 
-    handlePlus = () => {
-        this.setState((prev) => {
-            return {number: prev.number + 1}
-        });
+    // componentDidMount() {
+    //     this.interval = setInterval(() => {
+    //         this.setState(prevState => ({
+    //             timeInterval: prevState.timeInterval + 1
+    //         }))
+    //     }, 1000)
+    // }
+
+    componentWillUnmount() {
+        clearInterval(this.state.intervalId)
     }
 
-    handleMinus = () => {
-        this.setState((prev) => {
-            if (prev.number > 0) {
-                return {number: prev.number - 1}
-            } else {
-                return {number: prev.number}
-            }
-        });
-    }
 
 render() {
     return (
         <div>
             {this.state.visible && (
-                <>
-                    <h1>{this.identitiy.fullName}</h1>
-                    <h3>{this.identitiy.profession}</h3>
-                    <p>{this.identitiy.profession}</p>
-                    <img src={this.identitiy.img} alt=''/>
+                <div className='d-flex justify-content-center text-center bg-info-subtle mt-5 mx-5 rounded-5'>
+                    <div>
+                        <h1>{this.identitiy.fullName}</h1>
+                        <h3>{this.identitiy.profession}</h3>
+                        <p className='mx-3'>{this.identitiy.bio}</p>
+                        <div className='d-flex justify-content-center'>
+                            <img src={this.identitiy.img} alt=''/>
+                        </div>
 
-                    <button onClick={this.handleMinus}>-</button>
-                    <span>{this.state.number}</span>
-                    <button onClick={this.handlePlus}>+</button>
-                </>
+                        <p>Time Interval: {this.state.timeInterval} seconds</p>
+                    </div>
+                </div>
             )}
-            <button onClick={this.handleClick}>Show</button>
+
+                <div className='d-flex justify-content-center mt-3'>
+                    <button onClick={this.handleClick} className='rounded-3'>Show</button>
+                </div>
         </div>
     )
 }}
